5b91a5f406bc9e3d6d36d9ac587307750b6d4de7
y-tsuritani <ykt.tsuriya@gmail.com>
2024-09-08 06:21:27 +0900

    デプロイコマンドを修正

--- cloudrun/deploy.sh
+++ cloudrun/deploy.sh
@@ -1,7 +1,3 @@
-# docker build -t gcr.io/any-development/gcs-volume-mount-dev .
-
-# docker push gcr.io/any-development/gcs-volume-mount-dev
-
 gcloud run deploy gcs-volume-mount-dev \
   --source=./src \
   --region=us-central1 \
@@ -9,4 +5,5 @@ gcloud run deploy gcs-volume-mount-dev \
   --service-account test-run-gcs-mount \
   --add-volume=name=gcs-volume,type=cloud-storage,bucket=test-gcs-vol-mount \
   --add-volume-mount=volume=gcs-volume,mount-path=/mnt/gcs \
-  --update-env-vars=MOUNT_PATH=/mnt/gcs
+  --update-env-vars=MOUNT_PATH=/mnt/gcs,PROJECT_ID=any-development,TABLE_NAME=users,SOURCE_DATASET=TEST_DATA \
+  --memory=3Gi
